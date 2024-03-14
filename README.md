0c7569a227c221168fce9db11af9198ed295552d
Alfredo Tito <tito.alfred@hotmail.com>
2024-03-14 17:50:58 -0300

    feat: add new mixins

--- README.md
+++ README.md
@@ -45,3 +45,5 @@ Check out our [Next.js deployment documentation](https://nextjs.org/docs/deploym
 ```
 chmod ug+x .husky/*
 ```
+
+SCSS IntelliSense - mrmlnc
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className={main.teste}>
       <h2>DJA NEXT TEMPLATE</h2>
+      <p className={main.item1}>teste 1 2 3</p>
     </main>
   );
 }
