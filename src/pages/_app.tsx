39b51b889910f7315b03bf743e53e77e7724d8f3
Alfredo Tito Silva <tito.alfred@hotmail.com>
2024-04-01 15:31:10 -0300

    feat: add sass finally

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app';
 
 import '../styles/tokens.scss';
+import '../styles/styles.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,9 @@ import main from '../styles/main.module.scss';
 
 export default function Home() {
   return (
-    <main className={main.teste}>
-      <h2>DJA NEXT TEMPLATE</h2>
-      <p className={main.item1}>teste 1 2 3</p>
+    <main>
+      <h2 className={main.teste}>DJA NEXT TEMPLATE</h2>
+      <p className={main.item1}>eiwoghwogehowh</p>
     </main>
   );
 }
